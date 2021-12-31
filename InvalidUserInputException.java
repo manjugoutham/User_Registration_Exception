b62725bca4f3dd7e20a9b282e0b8028da97d0aa4
manjugoutham <gouthamy219@gmail.com>
2021-12-31 13:50:13 +0530

    throw custom exceptions in case of Invalid User Details

---
+++ InvalidUserInputException.java
@@ -0,0 +1,9 @@
+package com.java.regex.Regexuc1;
+
+public class InvalidUserInputException extends Exception {
+     
+	public InvalidUserInputException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
